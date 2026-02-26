dd1a4e7c013ce044f452ca1571d861733151a6b1
MD Faiz Khan <mdfaizkhan223@gmail.com>
2026-02-26 15:17:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,22 @@ import streamlit as st
 import pickle
 import re
 import string
+#ui
+
+st.set_page_config(
+    page_title="Fake News Detector",
+    page_icon="📰",
+    layout="centered"
+)
+
+st.markdown("""
+    <h1 style='text-align: center;'>📰 Fake News Detection System</h1>
+    <p style='text-align: center; color: grey;'>
+    AI Powered | Logistic Regression | TF-IDF
+    </p>
+""", unsafe_allow_html=True)
+
+st.divider()
 
 #load saved model
 model = pickle.load(open("model.pkl","rb"))
@@ -30,9 +46,14 @@ if st.button("check news"):
 
     if prediction[0] == 1:
         st.success("🟢 This is Real News")
-        st.write("Confidence:", round(probability[1]*100, 2), "%")
+        confidence = probability[1] * 100
     else:
         st.error("🔴 This is Fake News")
-        st.write("Confidence:", round(probability[0]*100, 2), "%")
+        confidence = probability[0] * 100
+
+    st.write(f"### Confidence Score: {round(confidence,2)}%")
+
+    st.progress(int(confidence))
+
 
 
